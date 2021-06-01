2629d900f4181daeea9691edd931e3ea8f0dee99
Alex <bigalen_2014@mail.ru>
2021-06-01 21:33:39 +0300

    Оптимизируем с помощью цикла

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+"use strict";
+
+
+
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -8,14 +13,18 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+
+        personalMovieDB.movies[a] = b;
+    }
+
+}
 
 console.log(personalMovieDB);
-
 
